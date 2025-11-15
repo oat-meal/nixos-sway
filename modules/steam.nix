8323c1e251b00c885deb0792b6de0b97b009e788
Christopher Seiber <christopher.seiber@gmail.com>
2025-11-14 21:18:52 -0600

    Update steam.nix

--- modules/steam.nix
+++ modules/steam.nix
@@ -2,56 +2,34 @@
 
 {
   ##############################
-  ## Steam Client + Proton GE ##
+  ## Steam client + Proton-GE ##
   ##############################
-
   programs.steam = {
     enable = true;
 
-    # Proton-GE for better compatibility
     extraCompatPackages = [ pkgs.proton-ge-bin ];
 
-    # Firewall exceptions for Remote Play, etc.
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = false;
   };
 
   ####################################
-  ## Vulkan, OpenGL, 32-bit Support ##
+  ## Vulkan / OpenGL / 32-bit stack ##
   ####################################
-
   hardware.graphics = {
-    enable = true;      # Enables Mesa/Vulkan GPU stack
-    enable32Bit = true; # REQUIRED for Steam + Proton
+    enable = true;      # Mesa/Vulkan stack
+    enable32Bit = true; # Needed for Proton / 32-bit games
   };
 
-  #########################################
-  ## Wayland + Steam Environment
-  #########################################
-
+  ####################################
+  ## Wayland + Steam session env    ##
+  ####################################
   environment.sessionVariables = {
-    # Wayland-native Steam UI / SDL
     SDL_VIDEODRIVER = "wayland";
     CLUTTER_BACKEND = "wayland";
     XDG_SESSION_TYPE = "wayland";
-
-    # Allow fallback for some Proton games
     QT_QPA_PLATFORM = "wayland;xcb";
-
-    # GPU vendor detection
     __GLX_VENDOR_LIBRARY_NAME = "mesa";
-
-    # Example: Steam UI scaling if needed
     STEAM_FORCE_DESKTOPUI_SCALING = "1";
   };
-
-  ##################
-  ## FHS Container ##
-  ##################
-  #
-  # This setting is global (see hosts/desktop.nix) and ensures
-  # unfree packages like Steam are allowed:
-  #
-  #   nixpkgs.config.allowUnfree = true;
-  #
 }
